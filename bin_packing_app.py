5c099211fb70070a1ab695e89b66c140c54a539b
Mason Christensen <mchristensen@dwavesys.com>
2022-03-28 15:12:24 -0700

    Add run button, readme placeholder, legend to plot

--- bin_packing_app.py
+++ bin_packing_app.py
@@ -1,5 +1,4 @@
 from io import StringIO
-from matplotlib.pyplot import plot
 import sys
 import streamlit as st
 from packing3d import (Cases,
@@ -9,7 +8,12 @@ from packing3d import (Cases,
                          call_cqm_solver)
 from utils import print_cqm_stats, plot_cuboids
 
-st.header("3D Bin Packing Demo")
+st.set_page_config(layout="wide")
+
+st.markdown(
+    "<h1 style='text-align: center;'>3D Bin Packing Demo</h1>",
+    unsafe_allow_html=True
+)
 
 problem_filepath = st.sidebar.text_input("Problem specification file",
                                          value="input/sample_data.txt")
@@ -18,34 +22,38 @@ pallet_width = st.sidebar.number_input("Pallet width", value=100)
 pallet_height = st.sidebar.number_input("Pallet height",value=110)
 num_pallets = st.sidebar.number_input("Number of pallets",value=1)
 time_limit = st.sidebar.number_input("Hybrid solver time limit (S)",value=20)
+run_button = st.sidebar.button("Run CQM Solver")
 
-cases = Cases(problem_filepath)
-pallets = Pallets(length=pallet_length,
-                  width=pallet_width,
-                  height=pallet_height,
-                  num_pallets=num_pallets,
-                  cases=cases)
+if run_button:
+    cases = Cases(problem_filepath)
+    pallets = Pallets(length=pallet_length,
+                    width=pallet_width,
+                    height=pallet_height,
+                    num_pallets=num_pallets,
+                    cases=cases)
 
-model_variables = Variables(cases, pallets)
+    model_variables = Variables(cases, pallets)
 
-cqm, origins = build_cqm(model_variables, pallets, cases)
+    cqm, origins = build_cqm(model_variables, pallets, cases)
 
-best_feasible = call_cqm_solver(cqm, time_limit)
+    best_feasible = call_cqm_solver(cqm, time_limit)
 
-#kwargs for ploty 3D meshes
-plot_kwargs = dict(alphahull=0, opacity=0.75, flatshading=True)
+    #kwargs for ploty 3D meshes
+    plot_kwargs = dict(alphahull=0, flatshading=True, showlegend=True)
 
-plotly_fig = plot_cuboids(best_feasible, model_variables, cases,
-                          pallets, origins, **plot_kwargs)
+    plotly_fig = plot_cuboids(best_feasible, model_variables, cases,
+                            pallets, origins, **plot_kwargs)
 
-st.plotly_chart(plotly_fig, use_container_width=True)
+    st.plotly_chart(plotly_fig, use_container_width=True)
 
-def _get_cqm_stats() -> str:
-    cqm_info_stream = StringIO()
-    sys.stdout = cqm_info_stream
-    print_cqm_stats(cqm)
-    sys.stdout = sys.__stdout__
+    def _get_cqm_stats() -> str:
+        cqm_info_stream = StringIO()
+        sys.stdout = cqm_info_stream
+        print_cqm_stats(cqm)
+        sys.stdout = sys.__stdout__
 
-    return cqm_info_stream.getvalue()
+        return cqm_info_stream.getvalue()
 
-st.code(_get_cqm_stats())
+    st.code(_get_cqm_stats())
+else:
+    st.text("Readme placeholder")
