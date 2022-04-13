b778bcfb44e6c8cb96479fe1fa28a58a9fa62ada
Mason Christensen <mchristensen@dwavesys.com>
2022-04-13 14:04:56 -0700

    display input data and add fields to random problem

--- bin_packing_app.py
+++ bin_packing_app.py
@@ -32,47 +32,18 @@ if run_type == "File upload":
                                              value="input/sample_data.txt")
     time_limit = st.sidebar.number_input(label="Hybrid solver time limit (S)",
                                          value=20)
+    display_input = st.sidebar.checkbox("Display input data")
     run_button = st.sidebar.button("Run CQM Solver")
 
-    if run_button:
-        data = read_instance(problem_filepath)
-        cases = Cases(data)
-        bins = Bins(data, cases=cases)
-
-        model_variables = Variables(cases, bins)
-
-        cqm, origins = build_cqm(model_variables, bins, cases)
-
-        best_feasible = call_cqm_solver(cqm, time_limit)
-
-        plotly_fig = plot_cuboids(best_feasible, model_variables, cases,
-                                  bins, origins)
-
-        st.plotly_chart(plotly_fig, use_container_width=True)
-
-        st.code(_get_cqm_stats(cqm))
-
-elif run_type == "Random":
     col1, col2 = st.columns([1,2])
+
     with col1:
-        with st.form(key="problem_config"):
-            time_limit = st.number_input(label="Hybrid solver time limit(S)",
-                                         value=20)
-            num_cases = st.slider("Number of unique case dimensions", 1, 5)
-            form_submit = st.form_submit_button("Run random problem")
-        
-    with col2:    
-        if form_submit:
-            rng = np.random.default_rng()
-            data = {
-                "num_bins":1,
-                "bin_dimensions":[100,100,100],
-                "quantity":rng.integers(8,12,num_cases),
-                "case_ids":[range(num_cases)],
-                "case_length":rng.integers(15,25,num_cases),
-                "case_width":rng.integers(15,25,num_cases),
-                "case_height":rng.integers(15,25,num_cases)
-            }
+        data = read_instance(problem_filepath)
+        if display_input:
+            st.write(data)
+
+    with col2:
+        if run_button:
             cases = Cases(data)
             bins = Bins(data, cases=cases)
 
@@ -83,8 +54,62 @@ elif run_type == "Random":
             best_feasible = call_cqm_solver(cqm, time_limit)
 
             plotly_fig = plot_cuboids(best_feasible, model_variables, cases,
-                                      bins, origins)
+                                    bins, origins)
 
-            st.plotly_chart(plotly_fig)
-    
+            st.plotly_chart(plotly_fig, use_container_width=True)
+
             st.code(_get_cqm_stats(cqm))
+
+elif run_type == "Random":
+    col1, col2 = st.columns([1,2])
+    with col1:
+        with st.form(key="problem_config"):
+            time_limit = st.number_input(label="Hybrid solver time limit(S)",
+                                         value=20)
+            num_bins = st.number_input("Number of bins", min_value=1,
+                                        max_value=3)
+            num_cases = st.number_input("Number of unique case types",
+                                        min_value=1, max_value=5)
+            bin_length = st.number_input("Bin length", min_value=50,
+                                         max_value=100)
+            bin_width = st.number_input("Bin width", min_value=50,
+                                        max_value=100)
+            bin_height = st.number_input("Bin height", min_value=50,
+                                         max_value=100)
+            form_submit = st.form_submit_button("Run random problem")
+
+        if form_submit:
+            rng = np.random.default_rng()
+
+            bin_volume = num_bins * bin_length * bin_width * bin_height
+            quantity = rng.integers(5,15,num_cases)
+            max_case_side_length = np.floor((bin_volume/sum(quantity))**(1/3))
+
+            data = {
+                "num_bins":num_bins,
+                "bin_dimensions":[bin_length, bin_width, bin_height],
+                "quantity":quantity,
+                "case_ids":np.array(range(num_cases)),
+                "case_length":rng.integers(10,max_case_side_length,num_cases),
+                "case_width":rng.integers(10,max_case_side_length,num_cases),
+                "case_height":rng.integers(10,max_case_side_length,num_cases)
+            }
+        
+            st.write(data)
+        
+            with col2:    
+                cases = Cases(data)
+                bins = Bins(data, cases=cases)
+
+                model_variables = Variables(cases, bins)
+
+                cqm, origins = build_cqm(model_variables, bins, cases)
+
+                best_feasible = call_cqm_solver(cqm, time_limit)
+
+                plotly_fig = plot_cuboids(best_feasible, model_variables,
+                                          cases, bins, origins)
+
+                st.plotly_chart(plotly_fig)
+        
+                st.code(_get_cqm_stats(cqm))
